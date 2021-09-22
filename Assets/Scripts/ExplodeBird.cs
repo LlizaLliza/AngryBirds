646f89489c07a2b98b551f429336f801eecbd1da
Uki Aula Fauzi <ukiaula39@gmail.com>
2021-09-22 09:32:04 +0700

    Add some features
    - Explode Bird
    - Win/Lose condition
    - Level 2

---
+++ Assets/Scripts/ExplodeBird.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeBird : Bird
+{
+    public float fieldoImpact;
+    public float force;
+    public GameObject ExplodeEffect;
+
+    public LayerMask LayerToHit;
+
+    public override void OnTap()
+    {
+        StartCoroutine(Explode(0f));
+        //Explode();
+    }
+
+    public override void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.tag == "Obstacle")
+            StartCoroutine(Explode(0.5f));
+        //Explode();
+    }
+
+    private IEnumerator Explode(float second)
+    {
+        yield return new WaitForSeconds(second);
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, fieldoImpact, LayerToHit);
+
+        foreach(Collider2D obj in objects)
+        {
+            Vector2 direction = obj.transform.position - transform.position;
+
+            obj.GetComponent<Rigidbody2D>().AddForce(direction * force);
+        }
+
+        GameObject ExplodeEffectIns = Instantiate(ExplodeEffect, transform.position, Quaternion.identity);
+        Destroy(ExplodeEffectIns, 1f);
+        Destroy(gameObject);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, fieldoImpact);
+    }
+}
